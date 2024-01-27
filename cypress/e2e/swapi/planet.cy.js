4e4ae0b75b1fc9268e2a7f178bd044dcd85cbcd9
lsdsubrosa <lsdsubrosa@gmail.com>
2024-01-27 21:48:27 +0300

    innit commit

---
+++ cypress/e2e/swapi/planet.cy.js
@@ -0,0 +1,40 @@
+import '@bahmutov/cy-api'
+
+describe('SWAPI', () => {
+    // контейнер для планет
+    context('planets data', () => {
+        it('check response, body', () => {
+            cy.request('/planets/').then((res) => {
+                // console.log('отладка', res.body.results.length) // отладка в консоли
+                expect(res.status).to.eq(200) // проверка статутса ответа
+                expect(res.body.results).length(10) // проверка кол-ва планет
+            })
+        })
+
+        it('tatooine data', () => {
+            cy.request('GET', '/planets/1').then((res) => {
+                // console.log('отладка', res.body.results[0]) // отладка в консоли
+                expect(res.body).to.have.property('name')
+                expect(res.body.name).to.be.a('string')
+                expect(res.body.name).to.have.length(8)
+                expect(res.body.name).to.eq('Tatooine')
+
+                expect(res.body).to.have.property('diameter')
+                expect(res.body.diameter).to.be.a('string')
+                expect(res.body.diameter).to.eq('10465')
+
+                expect(res.body.films).length(5)
+                expect(res.body.films[3]).to.eq('https://swapi.py4e.com/api/films/5/')
+            })
+        })
+
+        // плагин cy-api позволяет вывести тело ответа как в постмане или сделать POST запрос с телом
+        it('planets response json', () => {
+            cy.api({
+                method: 'GET', // POST
+                url: '/planets/1',
+                // body: {planetName: 'planet1'} // пример как могло бы быть
+            })
+        })
+    })
+})
