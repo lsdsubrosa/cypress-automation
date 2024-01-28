469e50534c874750fb6cba45374cebddfd5e4f34
lsdsubrosa <lsdsubrosa@gmail.com>
2024-01-28 18:08:30 +0300

    update config

--- cypress.config.js
+++ cypress.config.js
@@ -2,8 +2,5 @@ const {defineConfig} = require("cypress");
 
 module.exports = defineConfig({
     e2e: {
-        // baseUrl: 'https://swapi.py4e.com/api/',
-        // viewportWidth: 600,
-        // viewportHeight: 800
     },
 });
